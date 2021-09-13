6449b59668e9fd555d97cac7d97c6724c2e49992
Burkhalter Yannis <yannis.burkhalter@gmail.com>
2021-09-13 22:00:08 +0200

    Create '/aestetica' section

--- src/app/perso.module/components/me/me.component.spec.ts
+++ src/app/perso.module/components/me/me.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
-import { LangComponent } from './lang.component';
+import { MeComponent } from './me.component';
 
-describe('LangComponent', () => {
-  let component: LangComponent;
-  let fixture: ComponentFixture<LangComponent>;
+describe('MeComponent', () => {
+  let component: MeComponent;
+  let fixture: ComponentFixture<MeComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ LangComponent ]
+      declarations: [ MeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LangComponent);
+    fixture = TestBed.createComponent(MeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
