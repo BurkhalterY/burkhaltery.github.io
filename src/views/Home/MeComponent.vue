757d90b320949d920e5abdb08d295a0797144bf6
Yannis Burkhalter <yannis@burkhalter.dev>
2024-09-07 17:01:12 +0200

    I eat GPUs for breakfast.

--- src/views/Home/MeComponent.vue
+++ src/views/Home/MeComponent.vue
@@ -73,6 +73,7 @@ const contacts = [
             <template v-else>{{ info.value }}</template>
           </span>
         </li>
+        <li>I eat GPUs for breakfast.</li>
       </ul>
       <div class="flex justify-center gap-5">
         <a
