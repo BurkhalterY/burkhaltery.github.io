eb4055081e632a302cb84761c1792ffee1ea784d
Yannis Burkhalter <yannis@burkhalter.dev>
2025-02-11 22:49:20 +0100

    chore: move data to another file

---
+++ src/data/data.js
@@ -0,0 +1,227 @@
+export default [
+  {
+    type: "work",
+    name: "Open Net Sàrl",
+    location: "Lausanne, Suisse",
+    title: "Senior Odoo Developer",
+    start: "08/2021",
+    end: "Today",
+    image: "companies/open-net.svg",
+    skills: [
+      { name: "Odoo", icon: "odoo.svg" },
+      { name: "Python", icon: "python.svg" },
+      { name: "Vue.js", icon: "vuejs.svg" },
+    ],
+  },
+  {
+    type: "work",
+    name: "CPNV",
+    location: "Sainte-Croix, Suisse",
+    title: "Substitute Python Teacher",
+    start: "01/2025",
+    end: "03/2025",
+    description:
+      "Every Monday, I gave Python lessons to a first year apprentice class.",
+    image: "companies/cpnv.png",
+    skills: [{ name: "Python", icon: "python.svg" }],
+  },
+  {
+    type: "education",
+    name: "Federal Vocational Baccalaureate (FVB), Engineering, Architecture, Life Sciences",
+    school: "ETML - École technique, École des métiers de Lausanne",
+    start: "08/2022",
+    end: "06/2024",
+    image: "companies/etml.svg",
+    links: [
+      {
+        name: "Federal Vocational Baccalaureate",
+        url: "/files/etml/yb-mp.pdf",
+      },
+    ],
+  },
+  {
+    type: "education",
+    name: 'Workshop "Cryptocurrency for Integrators"',
+    school: "Hodling SA, Neuchâtel",
+    year: "10/2023",
+    image: "companies/hodling.svg",
+    links: [
+      {
+        name: "Certificate of completion",
+        url: "/files/hodling/yb-hodling.pdf",
+      },
+    ],
+  },
+  {
+    type: "education",
+    name: "Blockchain Specialization",
+    school: "University at Buffalo, Coursera",
+    start: "08/2023",
+    end: "12/2023",
+    image: "companies/coursera.svg",
+    links: [
+      {
+        name: "Blockchain Specialization",
+        url: "/files/coursera/yb-coursera-blockchain.pdf",
+      },
+      {
+        name: "Blockchain Basics",
+        url: "/files/coursera/yb-coursera-bc1.pdf",
+      },
+      {
+        name: "Smart Contracts",
+        url: "/files/coursera/yb-coursera-bc2.pdf",
+      },
+      {
+        name: "Decentralized Applications (Dapps)",
+        url: "/files/coursera/yb-coursera-bc3.pdf",
+      },
+      {
+        name: "Blockchain Platforms",
+        url: "/files/coursera/yb-coursera-bc4.pdf",
+      },
+    ],
+  },
+  {
+    type: "project",
+    name: "etml.burkhalter.dev",
+    description:
+      'The "digitized" version of my agenda from ETML, synchronized for the happiness of all my classmates.',
+    year: 2023,
+    image: "projects/etml-burkhalter-dev.png",
+    skills: [
+      { name: "Vue.js", icon: "vuejs.svg" },
+      { name: "Python", icon: "python.svg" },
+      { name: "GraphQL", icon: "graphql.svg" },
+    ],
+    links: [
+      {
+        name: "etml.burkhalter.dev",
+        url: "https://etml.burkhalter.dev/",
+      },
+      {
+        name: "frontend repository",
+        url: "https://github.com/BurkhalterY/etml.burkhalter.dev",
+      },
+      {
+        name: "backend repository",
+        url: "https://github.com/BurkhalterY/api.etml.burkhalter.dev",
+      },
+    ],
+  },
+  {
+    type: "education",
+    name: "Federal VET Diploma, Information Technologist, Specialism Application Development",
+    school: "EPSIC - École Professionnelle Lausanne",
+    start: "08/2018",
+    end: "07/2022",
+    image: "companies/epsic.svg",
+    links: [
+      {
+        name: "Federal VET Diploma",
+        url: "/files/epsic/yb-cfc.pdf",
+      },
+      {
+        name: "Gradebook",
+        url: "/files/epsic/yb-epsic-bulletin.pdf",
+      },
+    ],
+  },
+  {
+    type: "work",
+    name: "Orif",
+    location: "Pomy, Aigle, Suisse",
+    title: "Apprentice Developer",
+    start: "08/2017",
+    end: "07/2021",
+    image: "companies/orif.svg",
+    skills: [
+      { name: "Java", icon: "java.svg" },
+      { name: "PHP", icon: "php.svg" },
+      { name: "CodeIgniter", icon: "codeigniter.svg" },
+    ],
+  },
+  {
+    type: "project",
+    name: "Battleship",
+    description:
+      'A fully functional battleship game with numerous additional features like "sonars" and "nuclear bombs".',
+    year: 2019,
+    image: "projects/battleship.png",
+    skills: [{ name: "C#", icon: "csharp.svg" }],
+    links: [
+      {
+        name: "repository",
+        url: "https://github.com/BurkhalterY/EPSIC_Bataille_Navale",
+      },
+      {
+        name: "direct download",
+        url: "https://github.com/BurkhalterY/EPSIC_Bataille_Navale/releases/download/v1.2-beta/EPSIC_Bataille_Navale.exe",
+      },
+    ],
+  },
+  {
+    type: "project",
+    name: "Use Your Cheats",
+    description:
+      "A small Android application that exploits a security vulnerability in the Use Your Words backend. It allows you to vote for yourself and identify the house answer.",
+    year: 2019,
+    image: "projects/use-your-cheats.png",
+    skills: [
+      { name: "Android", icon: "android.svg" },
+      { name: "Java", icon: "java.svg" },
+      { name: "JavaScript", icon: "javascript.svg" },
+    ],
+    links: [
+      {
+        name: "repository",
+        url: "https://github.com/BurkhalterY/UseYourCheats",
+      },
+      {
+        name: "direct download (APK)",
+        url: "https://github.com/BurkhalterY/UseYourCheats/releases/download/1.1/use-your-words-1.1.apk",
+      },
+    ],
+  },
+  {
+    type: "project",
+    name: "epsic.burkhalter.dev",
+    description:
+      "A website designed for my former classmates. It includes summaries of some of our first-year apprenticeship courses at EPSIC.",
+    year: 2018,
+    image: "projects/epsic-burkhalter-dev.png",
+    skills: [{ name: "Vue.js", icon: "vuejs.svg" }],
+    links: [
+      {
+        name: "epsic.burkhalter.dev",
+        url: "https://epsic.burkhalter.dev",
+      },
+      {
+        name: "repository",
+        url: "https://github.com/BurkhalterY/epsic.burkhalter.dev",
+      },
+    ],
+  },
+  {
+    type: "project",
+    name: "Normalux.ch",
+    description:
+      "A drawing mini-game where you have to replicate a model in just 45 seconds. It has many modes and exploring the gallery can be very funny!",
+    year: 2018,
+    image: "projects/normalux.png",
+    skills: [
+      { name: "PHP", icon: "php.svg" },
+      { name: "CodeIgniter", icon: "codeigniter.svg" },
+    ],
+    links: [
+      {
+        name: "www.normalux.ch",
+        url: "https://www.normalux.ch/",
+      },
+      {
+        name: "repository",
+        url: "https://github.com/BurkhalterY/normalux.ch",
+      },
+    ],
+  },
+]
